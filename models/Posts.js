6a889d4d0c09e53f68f1d55a1010206c023773e9
Russ Wells <russ@russdigital.com>
2015-08-25 16:39:52 -0700

    Opening REST Routes Complete
    step complete

--- models/Posts.js
+++ models/Posts.js
@@ -1,20 +1,16 @@
 var mongoose = require('mongoose');
 
-var CommentSchema = new mongoose.Schema({
-	body: String,
-	author: String,
-	upvotes: {type: Number, default: 0},
-	post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
-});
-
-mongoose.model('Comment', CommentSchema);
-
 var PostSchema = new mongoose.Schema({
 	title: String,
 	link: String,
-	upvotes: {type: Number, default: 0}
+	upvotes: {type: Number, default: 0},
 	comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 });
+
+PostSchema.methods.upvote = function(cb) {
+	this.upvotes += 1;
+	this.save(cb);
+};
 
 mongoose.model('Post', PostSchema);
 
