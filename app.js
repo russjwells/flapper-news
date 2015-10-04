47a55bc90a3ef120bed014dbb169d86f5dd9785e
Russ Wells <russ@russdigital.com>
2015-10-04 16:36:35 -0700

    Adding Passport, Creating Authentication Endpoints, Securing Endpoints and Associating Posts and Comments with Users
    backend is ready

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
+var passport = require('passport');
+
 mongoose.connect('mongodb://localhost/news');
 
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -27,6 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('express-jwt');
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-var mongoose =require('mongoose');
+var mongoose = require('mongoose');
+var passport = require('passport');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
+
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+});
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
 
 router.get('/posts', function(req, res, next) {
 	Post.find(function(err, posts){
@@ -18,8 +56,9 @@ router.get('/posts', function(req, res, next) {
 	});
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts', auth, function(req, res, next) {
 	var post = new Post(req.body);
+	post.author = req.payload.username;
 
 	post.save(function(err, post){
 		if(err){ return next(err); }
@@ -49,7 +88,7 @@ router.param('comment', function(req, res, next, id) {
 		return next();
 	});
 });
-
+// return a post
 router.get('/posts/:post', function(req, res) {
 	req.post.populate('comments', function(err, post) {
 		if (err) { return next(err); }
@@ -58,7 +97,8 @@ router.get('/posts/:post', function(req, res) {
 	});
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+// upvote a post
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
 	req.post.upvote(function(err, post) {
 		if (err) { return next(err); }
 
@@ -66,17 +106,20 @@ router.put('/posts/:post/upvote', function(req, res, next) {
 	});
 });	
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+// upvote a comment
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
 	req.comment.upvote(function(err, comment) {
 		if (err) { return next(err); }
 
 		res.json(comment);
 	});
-});	
+});
 
-router.post('/posts/:post/comments', function(req, res, next) {
+// create a new comment
+router.post('/posts/:post/comments', auth, function(req, res, next) {
 	var comment = new Comment(req.body);
 	comment.post = req.post;
+	comment.author = req.payload.username;
 
 	comment.save(function(err, comment){
 		if(err){ return next(err); }
